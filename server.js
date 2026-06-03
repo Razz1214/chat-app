eaaf4c9cf260a90d6f28a6ea5b977d432a3f9577
Razz1214 <kumarazz7549@gmail.com>
2026-06-03 15:38:27 +0530

    Fix home route

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const io = new Server(server, {
 });
 
 app.use(express.static("public"));
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
 
 io.on("connection", (socket) => {
    console.log("User Connected:", socket.id);
